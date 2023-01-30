36aa6d3ab68d219ad5944b2632d9418a7da7ef5f
Rudhi Mediastara <rudhi.mediastara@gmail.com>
2023-01-30 10:54:31 +0700

    fix a11y input button

--- src/routes/FieldsetShell.svelte
+++ src/routes/FieldsetShell.svelte
@@ -29,6 +29,7 @@
 				class="sr-only"
 				type="button"
 				id="focus-point"
+				value="you are on the top of the page, press tab to the next field"
 			/>
 			<label class="sr-only" for="focus-point"
 				>focus point press tab to continue</label
